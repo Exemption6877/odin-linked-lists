fbbc9c198c9931e3007fd324b69e10470ad9360c
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-22 08:23:39 +0300

    Minor refactoring

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
 class LinkedList {
   constructor(head = null) {
     this.head = head;
@@ -5,25 +12,18 @@ class LinkedList {
 
   append(value) {
     const newNode = new Node(value);
-    function recursiveCheck(item) {
+    function findLast(item) {
       if (item.nextNode == null) {
         return item;
       } else {
-        return recursiveCheck(item.nextNode);
+        return findLast(item.nextNode);
       }
     }
     if (this.head == null) {
       this.head = newNode;
     } else {
-      const lastItem = recursiveCheck(this.head);
+      const lastItem = findLast(this.head);
       lastItem.nextNode = newNode;
     }
-  }
-}
-
-class Node {
-  constructor(value = null, nextNode = null) {
-    this.value = value;
-    this.nextNode = nextNode;
   }
 }
